9e5b6cdc39e08e97be54c0481371dea36719d3f7
Isabella C.S <isabellacs_tec@outlook.com>
2022-11-08 13:18:07 -0300

    Upload do prj total atualizado

---
+++ MariaDB/src/mariadb/Program.java
@@ -0,0 +1,13 @@
+package mariadb;
+
+
+/**
+ * Class Controller from the project. Start the project from this class.
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		loginGUI login = new loginGUI();
+		login.execute();
+	}
+}
